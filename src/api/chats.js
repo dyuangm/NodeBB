15c3bf1ea407e5da0f084ec68344808fea662c9c
Derek Yuan <dereky@andrew.cmu.edu>
2024-09-04 15:14:31 -0400

    fix commit

--- src/api/chats.js
+++ src/api/chats.js
@@ -151,7 +151,6 @@ chatsAPI.update = async (caller, data) => {
 		messaging.getRoomData(data.roomId),
 		user.isAdministrator(caller.uid),
 	]);
-	
 	if (!roomData) {
 		throw new Error('[[error:invalid-data]]');
 	}
